f76e94aac004a78a80d5ee6de03792dee6001ea3
Zhanghao <yukiniro@hotmail.com>
2025-11-26 10:18:41 +0800

    Update Tailwind CSS to version 4.1.17, remove Tailwind configuration file, and adjust CSS imports in App.css

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
-  tools: {
-    postcss: {
-      postcssOptions: {
-          plugins: [tailwindcss()],
-      }
-    }
-  },
 });
