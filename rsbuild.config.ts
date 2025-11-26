b609f5299ebb9a937b25c31fc44632f4fdbf13a2
Zhanghao <yukiniro@hotmail.com>
2025-11-26 09:55:56 +0800

    Add Tailwind CSS integration and update styles

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    postcss: {
+      postcssOptions: {
+          plugins: [tailwindcss()],
+      }
+    }
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div className="bg-blue-300 min-w-screen min-h-screen flex items-center justify-center flex-col text-white">
+      <h1 className="text-4xl font-bold">Rsbuild with React</h1>
+      <p className="text-2xl font-bold">Start building amazing things with Rsbuild.</p>
     </div>
   );
 };
