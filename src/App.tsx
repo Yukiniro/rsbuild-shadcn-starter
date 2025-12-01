725eef16734af3dae91727e17684bb5cddbdb748
Zhanghao <yukiniro@hotmail.com>
2025-12-01 16:43:12 +0800

    feat: integrate SVGR plugin and add badge icon to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import '@/styles/globals.css';
 import { Button } from '@/components/ui/button';
+import BadgeInfo from '@/assets/icons/badge-info.svg?react';
 
 const App = () => {
   return (
     <div className="bg-blue-300 min-w-screen min-h-screen flex items-center justify-center flex-col text-white">
       <h1 className="text-4xl font-bold">Rsbuild with React</h1>
       <p className="text-2xl font-bold mb-4">Start building amazing things with Rsbuild.</p>
-      <Button>Click me</Button>
+      <Button>
+        <BadgeInfo />
+        Click me
+      </Button>
     </div>
   );
 };
