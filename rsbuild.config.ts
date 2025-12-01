8bf5b6f877e8d9f7f477e8b978cceb09d1b8c3e6
Zhanghao <yukiniro@hotmail.com>
2025-12-01 16:43:51 +0800

    feat: integrate SVGR plugin and add badge icon to App component (#1)

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginSvgr()],
 });
